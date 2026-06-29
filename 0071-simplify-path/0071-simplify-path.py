819632df4050afb0dd9e60b3ba14a617fad4930a
Shanmukh Reddy <shanmukhreddy2507@gmail.com>
2026-06-29 08:51:39 +0530

    Time: 4 ms (34.87%), Space: 12.5 MB (59.2%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def simplifyPath(self, path):
+        stack = []
+        for part in path.split('/'):
+            if part == '' or part == '.':
+                continue
+            if part == '..':
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(part)
+        return '/' + '/'.join(stack)
