612c75134a0f3dab1b5a874d049ee9ae3936504b
Shanmukh Reddy <shanmukhreddy2507@gmail.com>
2026-06-29 08:54:54 +0530

    Time: 163 ms (36.18%), Space: 25.5 MB (7%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.py
@@ -0,0 +1,19 @@
+class MinStack(object):
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val):
+        if not self.stack:
+            self.stack.append((val, val))
+        else:
+            self.stack.append((val, min(val, self.stack[-1][1])))
+
+    def pop(self):
+        self.stack.pop()
+
+    def top(self):
+        return self.stack[-1][0]
+
+    def getMin(self):
+        return self.stack[-1][1]
